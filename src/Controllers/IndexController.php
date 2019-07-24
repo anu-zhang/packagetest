afd7aefa31a736f73b14143f52560ea87f832ec9
zhangmengke <michael_u@163.com>
2019-07-24 17:35:14 +0800

    大小写错误

--- src/Controllers/IndexController.php
+++ src/Controllers/IndexController.php
@@ -9,7 +9,6 @@ class IndexController extends BaseController
 {
     public function index()
     {
-
         //$data = 'Hello World';
         $data = Larapackage::getInfo();
         //可以直接使用创建的包里的视图，注意调用是两个冒号
